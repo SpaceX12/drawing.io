bbc8f99261de652bf4adefc127dcb3ad3e48d4db
ishan <scifyfuntime@gmail.com>
2020-05-18 16:12:41 +0530

    started from scratch and made th initial stuff work

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 var  database;
-var gameState = 0, playerCount, form, player, game;
+
+var drawing = [];
+var currentPath = [];
+
 
 function setup(){
   database = firebase.database();
@@ -7,11 +10,46 @@ function setup(){
 
   createCanvas(500,500);
 
-  game = new Game()
-  game.getState()
-  game.start();
+  canvas.mousePressed(startPath);
+  canvas.mouseRelesead(endPath);
 }
 
+function startPath(){
+  currentPath = [];
+  drawing.push(currentPath);
+
+}
+/*function endPath(){
+  
+}*/
+
+
 function draw(){
+  background(0);
     
+  if(mouseIsPressed){
+    var point = {
+
+      x: mouseX,
+      y: mouseY
+    }
+  }
+
+
+  stroke('yellow');
+  strokeWeight(2);
+  noFill();
+
+
+  for(var i = 0; i<drawing.length; i++){
+    var path = drawing[i];
+
+    beginShape();
+
+    for(var j = 0; j<path.length; j++){
+      vertex(drawing[j].x, drawing[j].y)
+    } 
+
+    endShape(); 
+  }
 }
